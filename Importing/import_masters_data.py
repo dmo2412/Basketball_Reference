1d78bb39a552509746adc0dd015aecb1de50e440
Danny Morgan <djmorgan2412@gmail.com>
2020-09-06 19:02:27 -0400

    Add importing of xl

---
+++ Importing/import_masters_data.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sqlalchemy import create_engine
+import sqlite3
+
+# connection = sqlite3.connect("firsty.db")
+# cursor = connection.cursor()
+
+xl_file = pd.ExcelFile('/Users/dannymorgan/Desktop/Masters_stats/masters_stats_2010_to_2019.xlsx')
+# print(xl_file.sheet_names)
+
+stats = xl_file.parse('Sheet1')
+
+db_uri = 'sqlite:///testing1.db'
+engine = create_engine(db_uri, echo=False)
+
+sample_db = stats.to_sql('testingdb1', con=engine)
+# sample_sql
+sample_sql = engine.execute("SELECT * FROM testing1.db").fetchall()
+print(sample_sql)
