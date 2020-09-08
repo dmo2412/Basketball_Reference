c08fb3a9b7af8158da719e49bde2c8178844d016
Danny Morgan <djmorgan2412@gmail.com>
2020-09-08 14:59:09 -0400

    gitignore

--- postgre.py
+++ postgre.py
@@ -14,20 +14,4 @@ print("Selecting rows from mobile table using cursor.fetchall")
 mobile_records = cursor.fetchall()
 postgreSQL_select_Query = "select * from pga_test2 where year = 2011"
 print(cursor.execute(postgreSQL_select_Query, (id,)))
-# print(mobile_records)
-# print("Print each row and it's columns values")
-# for row in mobile_records:
-#     print("Id = ", row[0], )
-#     print("Year = ", row[1])
-#     print("Player  = ", row[2], "\n")
-# print(postgreSQL_select_Query)
 
-# except (Exception, psycopg2.Error) as error:
-#     print("Error while fetching data from PostgreSQL", error)
-
-# finally:
-#     #closing database connection.
-#     if(connection):
-#         cursor.close()
-#         connection.close()
-#         print("PostgreSQL connection is closed")
