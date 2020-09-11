be21761fbf9b2e0cfab28f6dbac2912d3975d101
Danny Morgan <djmorgan2412@gmail.com>
2020-09-11 12:00:43 -0400

    Scrape by year instead of tournament

--- SQL/create_player_table.sql
+++ SQL/create_player_table.sql
@@ -6,12 +6,12 @@
 -- Where us_open.finish = 1 or masters.finish = 1 or pga_championship.finish = 1 or open_championship.finish = 1
 -- ORDER BY 
 
-SELECT player, count(finish) 
-from masters
-where finish < 3
-GROUP BY player
--- having count(finish) > 1
-order by 1;
+SELECT player, finish, fairways_rank, greens_hit_rank, putts_rank , driving_distance_rank
+from open_championship
+where finish = 1
+-- GROUP BY player
+-- -- having count(finish) > 1
+order by year DESC;
 
 -- Create masters table with each players stats
 -- Create table for all finishes
