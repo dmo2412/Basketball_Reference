87cefab13ec5ad2e93829f2e1c5a2d58d7d23d23
Danny Morgan <djmorgan2412@gmail.com>
2020-09-14 11:37:46 -0400

    Add us_open_tables

--- SQL/us_open_finish.sql
+++ SQL/us_open_finish.sql
@@ -1,7 +1,7 @@
 DROP TABLE usopenstats19;
 
 CREATE TABLE usopenstats19 AS       
-SELECT stats19.player, us_open.finish, stats19.greens,us_open.greens_hit , 
+SELECT stats19.player, us_open.finish, stats19.greens,us_open.greens_hit_rank AS usopen_greens , 
 stats19.putt_rank, us_open.putts_rank AS usopen_putts, stats19.up_and_down, stats19.distance, us_open.driving_distance AS usopen_distance,
 stats19.fairways, us_open.fairways_rank AS usopen_fairways
 FROM stats19
